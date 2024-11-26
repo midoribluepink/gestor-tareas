49495e0574edc8a3db8fc9ef0daa6547976298ec
midoribluepink <midoribluepink@localhost.localdomain>
2024-11-26 14:35:42 -0600

    Creación de la función del bucle principal, menú y función de creación de tareas

--- gestor_tareas.py
+++ gestor_tareas.py
@@ -30,4 +30,40 @@ class Tarea():
 
         self.contenido = contenido
 
+#Función para mostar el menú de opciones
+def mostrar_menu():
+    print("\n[+] ¡Bienvenido al gestor de tareas!\n")
+    print("Opciones:")
+    print("1: Salir del programa")
+    print("2: Crear una nueva tarea")
+
+#Función par añadir una nueva tarea a la lista
+def crear_tarea():
+    print("\n[+] ¡Vamos a crear una nueva tarea!\n")
+    titulo = input("Inserte el título de la nueva tarea: \n") #Solicitamos el título de la tarea
+    contenido = input("Inserte el contenido de la tarea: \n") #Solicitamos el contenido de la tarea
+    status = input("Inserte el status de la tarea (True/False)\n") #Pedimos el status de la tarea
+    numero_de_tarea = str(len(tareas) + 1) #Calculamos el número de tarea
+
+    tareas["tarea"+numero_de_tarea] = Tarea(titulo, contenido, status) #Creamos la nueva tarea
+
+    print(f"\n[+] Se ha creado la tarea '{'tarea'+numero_de_tarea}':\n")
+    print(tareas["tarea"+numero_de_tarea])
+
+tareas = {} #Set donde se guardarán las tareas
+
+# Bucle principal del programa
+while True:
+    
+    mostrar_menu() #Mostramos el menú de opciones
+
+    opcion = int(input("\n¿Qué es lo que desea hacer?\n")) #Solicitamos la instrucción de ejecución
+
+    if opcion == 1:
+        # Opción para cerrar el programa 
+        print("\n[!] Saliendo del programa...\n")
+        break
+    elif opcion == 2:
+        # Opción para crear la tarea
+        crear_tarea()
 
